145f0c53b4d31b84d11854ab2b632b0c4eff866b
tindleaj <tindleaj@gmail.com>
2017-01-09 10:04:48 -0500

    changed registration pricing

--- register.php
+++ register.php
@@ -18,8 +18,8 @@ $page_title = 'Home';
 		<h6><i>The registration fee includes coffee, lunch on both days, and reception during the poster presentation</i></h6>
         <h6>Early bird registration by Jan 20, 2017</h6>
         <ul>
-        	<li>Regular: $200</li>
-        	<li>Student: $100</li>
+        	<li>Regular: $100</li>
+        	<li>Student: $50</li>
         </ul>
 
         <h6>Registration after Jan 20, 2017</h6>
