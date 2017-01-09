cfee5555001a568cad7c7e6d90b036ee0c71de17
tindleaj <tindleaj@gmail.com>
2017-01-09 10:04:14 -0500

    added sponsor section, styles, and images to homepage

--- index.php
+++ index.php
@@ -14,8 +14,53 @@ $page_title = 'Home';
     </div>
 
     <div class="seven columns">
+        <!-- Overview -->
         <h4>February 3-4, 2017 | Orlando, FL</h4>
         <p>2D materials beyond graphene are receiving enormous attention due to their potential in revolutionizing our understanding of fundamental sciences as well as our ability to push the limits of electronics for applications in nanodevices and smart materials. This <strong>2nd International Symposium on Science and Technology of 2D materials</strong> aims to create an environment where leading experts in the field, young researchers and students involved in all aspects of 2D materials research can start a dialog, develop new knowledge and foster new ideas that will shape the future of 2D materials research. The workshop will feature Lectures by leading scientists in the field of 2D materials to present advances at the forefront of the field.</p>
+
+        <div class="sponsors">
+            <!-- Sponsors -->
+            <h4>Our Sponsors</h4>
+
+            <!-- Platinum -->
+            <h5>Platinum Sponsors ($1000+)</h5>
+
+            <div class="platinum">
+                <!-- Research and Commercialization -->
+                <img src="images/sponsors/research.png">
+                <!-- Graduate Studies -->
+                <img src="images/sponsors/gradstudies.png">
+                <!-- College of Sciences -->
+                <img src="images/sponsors/sciences.png">
+                <!-- CREOL -->
+                <img src="images/sponsors/creol.png">
+                <!-- Ultratech -->
+                <img src="images/sponsors/ultratech.png">
+                <!-- Nova -->
+                <img src="images/sponsors/nova.png">
+            </div>
+
+            <!-- Silver -->
+            <h5>Silver Sponsors ($500+)</h5>
+
+            <div class="silver">
+                <!-- Engineering and Compsci -->
+                <img src="images/sponsors/research.png">
+                <!-- Thorlabs -->
+                <img src="images/sponsors/thorlabs.png">
+                <!-- Itechu -->
+                <img src="images/sponsors/itechu.png">
+                <!-- Mantis Sigma -->
+                <img src="images/sponsors/mantissigma.png">
+                <!-- Femto Science -->
+                <img src="images/sponsors/femtoscience.png">
+                <!-- Scienta Omicron -->
+                <img src="images/sponsors/scientaomicron.png">
+                <!-- 2D Materials Journal -->
+                <img src="images/sponsors/2dmaterials.png">
+            </div>
+
+        </div>
     </div>
 
     <div class="three columns">
